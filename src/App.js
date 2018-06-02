4bb3aca878b7ea50041067010f012b5190b53d26
RaufR <rauf.rahmancz@gmail.com>
2018-06-02 19:09:49 +0200

    ad type parameter for different query+css simplification

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
         this.state = {
             term: '',
             img: '',
-            title: ''
+            title: '',
+            type: ''
         };
     }
 
@@ -21,7 +22,7 @@ class App extends Component {
         const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.term}&api_key=${api_key}`;
         fetch(url)
             .then(response => response.json())
-            .then(data => this.setState({ term:'', img: data.data[0].images.fixed_height.url, title: data.data[0].title }))
+            .then(data => this.setState({ term:'', img: data.data[0].images.fixed_height.url, title: data.data[0].title, type: data.data[0].type }))
             .then(console.log(this.title))
             .then(console.log(this.data))
             .catch(e => console.log('error', e));
@@ -31,11 +32,16 @@ class App extends Component {
         return (
             <div className="App">
                 <form onSubmit={this.handleSubmit}>
-                    <input value={this.state.term} onChange={this.onChange} />
+                    <div>
+                        <label>Search: </label>
+                        <input className="highlight" value={this.state.term} onChange={this.onChange} />
+                    </div>
+
                     <button>Submit!</button>
                 </form>
                 <img src={this.state.img} height="200" alt={this.state.term} />
-                <h1>NAME: {this.state.title}</h1>
+                <h1> {this.state.title}</h1>
+                <p> {this.state.type}</p>
             </div>
         );
     }
